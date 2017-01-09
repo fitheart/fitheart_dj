dc63bd9d61df4e87f9caf89260ab290cd9d54341
bichkhe <mr.bichkhe@gmail.com>
2017-01-09 14:41:20 +0700

    Create nav bar from database

--- fitheart/models.py
+++ fitheart/models.py
@@ -2,36 +2,31 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+# Create your models here.
+class ProductGroup(models.Model):
+    name = models.CharField(max_length=100, default="")
+    #products = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return ' '.join([
+            self.name,
+        ])
+
+
 class Product(models.Model):
     """
         Product
     """
     name = models.CharField(max_length=100, default="")
     model = models.CharField(max_length=100,  default="")
-    url = models.URLField( default="")
+    #url = models.URLField( default="")
     description = models.TextField(max_length=200,  default="")
-    def __init__(self):
-        self.name = ""
-        self.model = ""
-        self.description = ""
-
+    product_group = models.ForeignKey(ProductGroup, on_delete=models.CASCADE)
     def __str__(self):
         return ' '.join([
             self.name,
             self.model,
         ])
 
-
-# Create your models here.
-class ProductGroup(models.Model):
-    name = models.CharField(max_length=100)
-    products = models.ForeignKey(Product, on_delete=models.CASCADE)
-    def __init__(self):
-        self.name = ""
-
-    def __str__(self):
-        return ' '.join([
-            self.name,
-        ])
 
 
--- fitheart/views.py
+++ fitheart/views.py
@@ -5,15 +5,19 @@ from django.views.generic.edit import CreateView, BaseCreateView, BaseDetailView
 from django.views.generic import ListView
 from django.forms import Form
 from django.http import JsonResponse
+from .models import (ProductGroup, Product)
 
 # Create your views here.
 
 
 def index(request):
     template = loader.get_template('fitheart/index.html')
+    product_grp_list = ProductGroup.objects.all()
+
     context ={
+        'product_grp_list': product_grp_list,
     }
-    return render(request, 'fitheart/index.html', context)
+    return render(request, 'fitheart/index.html', context=context)
 
 def product_reviews(request):
     template = loader.get_template('fitheart/product_reviews.html')
