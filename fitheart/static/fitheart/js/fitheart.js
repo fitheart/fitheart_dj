9104876be6ca0c79f2d30e57a206c39fa2e6f76a
bichkhe <mr.bichkhe@gmail.com>
2017-02-04 14:05:15 +0700

    add angular js

--- fitheart/static/fitheart/js/fitheart.js
+++ fitheart/static/fitheart/js/fitheart.js
@@ -2,81 +2,25 @@
  * Created by bichkhe on 7/2/2016.
  */
 
-//sign_up in page_header.html
-
-jQuery(document).ready(function (result) {
-    $('#login-form').on('submit', function(event){
-    event.preventDefault();
-    console.log("form submitted!-----------------------") ; // sanity check
-    $('#dialog').dialog();
-    $.ajax({
-        url : "/music/login", // the endpoint
-        type : "POST", // http method
-        data : {'value':'123456'}, // data sent with the post request
-
-        // handle a successful response
-        success : function(json) {
-            console.log(json); // log the returned json to the console
-            console.log("success"); // another sanity check
-        },
-
-        // handle a non-successful response
-        error : function(xhr,errmsg,err) {
-            console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
-        }
-    });
+var app = angular.module('fitheart', []);
+app.config(function($httpProvider) {
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 });
-})
-// $('#login-form').on('submit', function(event){
-//     event.preventDefault();
-//     console.log("form submitted!-----------------------") ; // sanity check
-//     $('#dialog').dialog();
-//     $.ajax({
-//         url : "login", // the endpoint
-//         type : "POST", // http method
-//         data : {'value':'123456'}, // data sent with the post request
-//
-//         // handle a successful response
-//         success : function(json) {
-//             console.log(json); // log the returned json to the console
-//             console.log("success"); // another sanity check
-//         },
-//
-//         // handle a non-successful response
-//         error : function(xhr,errmsg,err) {
-//             console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
-//         }
-//     });
-// });
-
-function onclick1() {
-    console.log('Onclick1');
-    alert('button is clicked!~');
-    jQuery.ajax({
-        method: 'post',
-        url: 'login',
-        success: function (result) {
-            alert(result);
-        },
-        error: function (msg) {
-            alert(msg);
-
-        }
-    });
-}
-
-
-//Test for click button
-function  ClickProductGroup(id) {
-    console.log('ClickProductGroup with id=' + id);
-    //alert('ClickProductGroup!~');
-}
-
-// Show article content
-function ShowArticleContent(content) {
-     console.log("ShowArticleContent(), Show html content") ; // sanity check
-    $("#article_content").html(content);
-}
-
-
-
+app.controller("FitHeartController", function ($scope, $http) {
+    $scope.helloTo = {};
+    $scope.helloTo.title = "AngularJS";
+    $scope.firstname = "Phung";
+    $scope.lastname = "Tuan";
+    $scope.save = function ($modal, $id) {
+        console.log("CLICKED SAVE!");
+        $http({
+            method: "POST",
+            url: "/product_reviews/"
+        }).then(function mySucces(response) {
+            $scope.myWelcome = response.data;
+        }, function myError(response) {
+            $scope.myWelcome = response.statusText;
+        });
+    }
+});
